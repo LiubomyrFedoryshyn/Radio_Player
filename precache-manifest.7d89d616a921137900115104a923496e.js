e80646592d6d0e8ede8f38cfd9cee57eb066e63b
robber <robber995@gmail.com>
2019-04-21 12:23:15 +0300

    Updates

---
+++ precache-manifest.7d89d616a921137900115104a923496e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75a956d0547b1553bb8f",
+    "url": "/Radio_Player/static/js/runtime~main.75a956d0.js"
+  },
+  {
+    "revision": "4046e7dc2f610528a8c4",
+    "url": "/Radio_Player/static/js/main.4046e7dc.chunk.js"
+  },
+  {
+    "revision": "54cd7d8350019e873c58",
+    "url": "/Radio_Player/static/js/1.54cd7d83.chunk.js"
+  },
+  {
+    "revision": "4046e7dc2f610528a8c4",
+    "url": "/Radio_Player/static/css/main.3f27080b.chunk.css"
+  },
+  {
+    "revision": "68c9044eb19b6db4d89ce475b4b93830",
+    "url": "/Radio_Player/index.html"
+  }
+];
